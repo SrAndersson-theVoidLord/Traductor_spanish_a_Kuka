39e662a4b76edfb6777b4bfb6d1ab2af0d6ab8b2
Lucas Greco <grecolucas477@yahoo.com.ar>
2025-04-21 23:44:32 -0300

    Initial commit
    Traductor de tipo satira a lenguaje para comunicarte con tus amigos de Argensimia

---
+++ main.js
@@ -0,0 +1,36 @@
+function traducir() {
+    const texto = document.getElementById('sourceText').value;
+    const idioma = document.getElementById('languageSelect').value;
+    let resultado = texto;
+
+    switch (idioma) {
+        case 'kircho':
+            resultado = texto.replace(/[aeiouáéíóú]/gi, function(match) {
+                return match === match.toLowerCase() ? 'i' : 'I';
+            });
+            break;
+        case 'feminista':
+            resultado = lenguajeInclusivo(texto);
+            break;
+        case 'zurdo':
+            resultado = zurdoMode();
+            break;
+        default:
+            resultado = texto;
+    }
+
+    document.getElementById('translatedText').value = resultado;
+}
+
+function lenguajeInclusivo(texto) {
+    return texto.replace(/\b(\w*?)o\b/gi, function(match, p1) {
+        return p1 + 'e';
+    }).replace(/\b(\w*?)os\b/gi, function(match, p1) {
+        return p1 + 'es';
+    });
+
+}
+
+function zurdoMode() {
+    return "exproiese, muerte al kapitalismo, la emision es buena, muere empresario cerdo microcapitalista cis, muchas más incongruencias, etc, etc, (hay mucho olor a culo y porro)";
+} 
